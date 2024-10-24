2b8f183abef083c48c0f1876111d00b573fc6378
faiz21s04 <faizkelas21s04@gmail.com>
2024-10-24 17:28:26 +0700

    commit

---
+++ app/src/test/java/com/faizmuarif/uts_faizmuarif/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.faizmuarif.uts_faizmuarif;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
